f605d588f03de3fd288144292a1e233cbae57b7b
PioneerSwine996 <pioneerswine9@gmail.com>
2025-07-14 10:58:00 -0700

    switching

--- UnityProject/Assets/Scripts/Movement.cs
+++ UnityProject/Assets/Scripts/Movement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float sensitivity;
     private float xRotation;
 
+    Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {}
@@ -18,67 +20,74 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
     private void Move()
     {
-        Vector2 forceDir = Vector2.zero;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        // Vector2 forceDir = Vector2.zero;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            forceDir += new Vector2((transform.forward).x, (transform.forward).z);
-            Debug.Log("Moving Forward");
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            forceDir = new Vector2((-transform.forward).x, (-transform.forward).z);
-            Debug.Log("Moving Backward");
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            forceDir = new Vector2((transform.right).x, (transform.right).z);
-            Debug.Log("Moving Right");
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            forceDir = new Vector2((-transform.right).x, (-transform.right).z);
-            Debug.Log("Moving Left");
-        }
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))
-        {
-            forceDir = Vector2.zero;
-            Debug.Log("Not Moving Forward or Backward");
-        }
-        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
-        {
-            forceDir = Vector2.zero;
-            Debug.Log("Not Moving Left or Right");
-        }
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
-        {
-            forceDir = new Vector2((transform.forward - transform.right).x, (transform.forward - transform.right).z);
-            Debug.Log("Moving Forward and Left");
-        }
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
-        {
-            forceDir = new Vector2((transform.forward + transform.right).x, (transform.forward + transform.right).z);
-            Debug.Log("Moving Forward and Right");
-        }
-        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
-        {
-            forceDir = new Vector2((-transform.forward - transform.right).x, (-transform.forward - transform.right).z);
-            Debug.Log("Moving Backward and Left");
-        }
-        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
-        {
-            forceDir = new Vector2((-transform.forward + transform.right).x, (-transform.forward + transform.right).z);
-            Debug.Log("Moving Backward and Right");
-        }
-        if (forceDir.magnitude < .01f)
-        {
-            rb.AddForce(-rb.linearVelocity);
-        }
-        else
-        {
-            rb.AddForce(forceDir.normalized * Mathf.Max(0, (moveSpeed - rb.linearVelocity.magnitude)));
-        }
+        // if (Input.GetKey(KeyCode.W))
+        // {
+        //     forceDir += new Vector2((transform.forward).x, (transform.forward).z);
+        //     Debug.Log("Moving Forward");
+        // }
+        // if (Input.GetKey(KeyCode.S))
+        // {
+        //     forceDir = new Vector2((-transform.forward).x, (-transform.forward).z);
+        //     Debug.Log("Moving Backward");
+        // }
+        // if (Input.GetKey(KeyCode.D))
+        // {
+        //     forceDir = new Vector2((transform.right).x, (transform.right).z);
+        //     Debug.Log("Moving Right");
+        // }
+        // if (Input.GetKey(KeyCode.A))
+        // {
+        //     forceDir = new Vector2((-transform.right).x, (-transform.right).z);
+        //     Debug.Log("Moving Left");
+        // }
+        // if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))
+        // {
+        //     forceDir = Vector2.zero;
+        //     Debug.Log("Not Moving Forward or Backward");
+        // }
+        // if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+        // {
+        //     forceDir = Vector2.zero;
+        //     Debug.Log("Not Moving Left or Right");
+        // }
+        // if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        // {
+        //     forceDir = new Vector2((transform.forward - transform.right).x, (transform.forward - transform.right).z);
+        //     Debug.Log("Moving Forward and Left");
+        // }
+        // if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        // {
+        //     forceDir = new Vector2((transform.forward + transform.right).x, (transform.forward + transform.right).z);
+        //     Debug.Log("Moving Forward and Right");
+        // }
+        // if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        // {
+        //     forceDir = new Vector2((-transform.forward - transform.right).x, (-transform.forward - transform.right).z);
+        //     Debug.Log("Moving Backward and Left");
+        // }
+        // if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        // {
+        //     forceDir = new Vector2((-transform.forward + transform.right).x, (-transform.forward + transform.right).z);
+        //     Debug.Log("Moving Backward and Right");
+        // }
+        // if (forceDir.magnitude < .01f)
+        // {
+        //     rb.AddForce(-rb.linearVelocity);
+        // }
+        // else
+        // {
+        //     rb.AddForce(forceDir.normalized * Mathf.Max(0, (moveSpeed - rb.linearVelocity.magnitude)));
+        // }
     }
 }
